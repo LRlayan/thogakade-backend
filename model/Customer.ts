da4058469ce0a76ab68afe9773eb42fe61531833
LRLayan <rameshlayan4@gmail.com>
2025-01-24 03:03:34 +0530

    refactor Customer.ts

--- model/Customer.ts
+++ model/Customer.ts
@@ -1,19 +1,21 @@
-import {Orders} from "@prisma/client";
+import Orders from "./Order";
 
 class Customer{
-    id:string;
+    id:number;
+    customerId:string;
     name:string;
-    nic:string;
+    address:string;
     email:string;
-    phone:string;
     orders:Orders[];
 
-    constructor(id: string, name: string, nic: string, email: string, phone: string, orders: Orders[]) {
+    constructor(id: number, customerId: string, name: string, address: string, email: string, orders: Orders[]) {
         this.id = id;
+        this.customerId = customerId;
         this.name = name;
-        this.nic = nic;
+        this.address = address;
         this.email = email;
-        this.phone = phone;
         this.orders = orders;
     }
 }
+
+export default Customer;
