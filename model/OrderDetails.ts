5982369b279ad69670cddd5afa4203c7124f8b85
LRLayan <rameshlayan4@gmail.com>
2025-01-23 23:30:15 +0530

    orderDetails model

---
+++ model/OrderDetails.ts
@@ -0,0 +1,15 @@
+class OrderDetails {
+    orderDetailId: number;
+    orderId: number;
+    itemId: number;
+    quantity: number;
+    price : number;
+
+    constructor(orderDetailId: number, orderId: number, itemId: number, quantity: number, price: number) {
+        this.orderDetailId = orderDetailId;
+        this.orderId = orderId;
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
