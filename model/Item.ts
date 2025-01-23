e0c3bd93d96531f86795b058023e53a96866dd8b
LRLayan <rameshlayan4@gmail.com>
2025-01-23 20:17:03 +0530

    item model

---
+++ model/Item.ts
@@ -0,0 +1,13 @@
+class Item{
+    id:string;
+    name:string;
+    quantity:number;
+    price:number;
+
+    constructor(id: string, name: string, quantity: number, price: number) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
